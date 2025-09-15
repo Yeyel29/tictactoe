2727d6aa70e10f46f52c916b27e200c14a67a314
Daniel Nino S. Catoy <daniuscatoy@gmail.com>
2025-09-15 13:49:50 +0800

    Add game logic with state and winner detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import React, { useState } from "react";
-import Board from "./components/Board";
+import React from "react";
+import Game from "./components/Game";
 
 function App() {
-  const [squares, setSquares] = useState<( "X" | "O" | null )[]>(Array(9).fill(null));
-
-  function handleClick(i: number) {
-    const newSquares = squares.slice();
-    newSquares[i] = "X";
-    setSquares(newSquares);
-  }
-
-  return <Board squares={squares} onClick={handleClick} />;
+  return <Game />;
 }
 
 export default App;
